a7749ef3ab9b0ab8b05039e51f4890fada2ba48a
Roma <romchalol123@gmail.com>
2022-10-18 19:05:52 +0300

    1

--- hw-1-operators-statements/index.js
+++ hw-1-operators-statements/index.js
@@ -69,36 +69,21 @@ const findTriangleType = (side1, side2, side3) => {
  */
 //function should return amount of days in month
 const findDaysInMonth = (month, year) => {
+
+  let days;
   
-  switch (month){
-    case 1:
-    case 3:
-    case 5:
-    case 7:
-    case 8:
-    case 10:
-    case 12:
-      days = 31;
-      break;
-
-    case 4:
-    case 6:
-    case 9:
-    case 11:
-      days = 30;
-      break;
-
-    case 2:
-      if( year % 4 === 0){
-        days = 29;
-      } else{
-        days = 28;
-      }
-      break;
-    
-    default:
-      return `Invalid Month of value ${month}`;
-
+  if (month < 1 || month > 12){
+    return `Invalid Month of value ${month}`
+  } else if (month === 2){
+    if(year % 4 === 0){
+      days = 29;
+    } else{
+      days = 28;
+    }
+  } else if(month === 4 || month === 6 || month === 9 || month === 11){
+    days = 30;
+  } else{
+    days = 31;
   }
 
   return `The Month has ${days} days`;
