c9bb670701fedf5a54a30cd59c27685fc3cec00b
Roma <romchalol123@gmail.com>
2022-10-27 19:53:34 +0300

    r

---
+++ hw-3-data-transformation/index.js
@@ -0,0 +1,120 @@
+/**
+ * @param {string} str
+ * @return {string}
+ */
+// function returns string without spaces from the beginning and from the end, and in upper letter register
+const transformString = (str) => {
+  // write your code here
+};
+
+/**
+ * @param {number[]} array
+ * @return {number}
+ */
+// function should return max number from array
+const findMaxNumber = (array) => {
+  // write your code here
+};
+
+
+/**
+ * @param {string} str
+ * @return {number[]}
+ */
+// function returns array of length of every word in string
+const getStringWordsLength = (str) => {
+  // write your code here
+};
+
+/**
+ * @param {number[]} numArray
+ * @param {number} degree
+ * @return {number[]}
+ */
+// function returns array of numbers as result of initial number and degree
+const getTransformedNumbers = (numArray, degree) => {
+  // write you code here
+};
+
+/**
+ * @param {string} text
+ * @return {string}
+ */
+// function returns text with all first letters at the beginning of sentence capitalized
+const getTransformedText = (text) => {
+  // write your code here
+};
+
+/**
+ * @param {any[]} array
+ * @return {number[]}
+ */
+// function filters array and return only array of positive integers
+const getPositiveIntegers = (array) => {
+  // write your code here
+};
+
+/**
+ * @param {any[]} array
+ * @param {any} value
+ * @return {number}
+ */
+// functions return index of element in array
+const getElementIndex = (array, value) => {
+  // write your code here
+};
+
+/**
+ * @param {any[]} array
+ * @param {any} value
+ * @return {any | null}
+ */
+// function returns item from array or undefined if item is not found
+const getItem = (array, value) => {
+  // write your code here
+};
+
+/**
+ * @param {string[]} array
+ * @param {string} word
+ * @return {boolean}
+ */
+// function returns true if word is in every string in array and false if is not
+const isWordInEveryArrayString = (array, word) => {
+  // write your code here
+};
+
+/**
+ * @param {number[]} array
+ * @return {boolean}
+ */
+// function returns true if any number in array is negative
+const isNegativeNumbersInArray = (array) => {
+  // write your code here
+};
+
+/**
+ * @param {number[]} array
+ * @param {number} startPosition
+ * @param {number} endPosition
+ * @return {any[]}
+ */
+// function returns part of array from start to end (including end) positions
+const returnArrayPart = (array, startPosition, endPosition) => {
+  // write your code here
+};
+
+
+module.exports = {
+  transformString,
+  findMaxNumber,
+  getStringWordsLength,
+  getTransformedNumbers,
+  getTransformedText,
+  getPositiveIntegers,
+  getElementIndex,
+  getItem,
+  isWordInEveryArrayString,
+  isNegativeNumbersInArray,
+  returnArrayPart,
+};
