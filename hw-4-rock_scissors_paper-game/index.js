43f734ab2b40c94c3b090c37a9297e8b65af7178
Roma <romchalol123@gmail.com>
2022-11-06 03:57:58 +0300

    hw-4 is done

--- hw-4-rock_scissors_paper-game/index.js
+++ hw-4-rock_scissors_paper-game/index.js
@@ -1,9 +1,3 @@
-function gameStarted(){
-
-    fighting();
-
-}
-
 function enterUserName(){
     let userName = prompt('Please, enter your name');
     if (userName === '' || userName === ' ' || userName === null){
@@ -20,7 +14,7 @@ function compChoice(){
         return res;
     }
 
-    num = randomChoice(0, moveVars.length - 1);
+    num = randomChoice(0, moveVars.length - 2);
 
     alert(`Computer move is: ${moveVars[num]}`);
     return moveVars[num];
@@ -32,7 +26,7 @@ function playerChoice(){
     if(choice === null){
         alert('You aborted this game. To start new game just refresh the page.');
         process.abort();
-    }else if (moveVars.includes(choice) === true){
+    }else if (!!moveVars.includes(choice)){
         return choice;
     } else{
         return playerChoice();
@@ -40,47 +34,51 @@ function playerChoice(){
 }
 
 function fighting(){
-
     let plrPoints = 0;
     let compPoints = 0;    
     
-    while (plrPoints < 3 || compPoints < 3){
+    while (plrPoints < 3 && compPoints < 3){
         let playerMove = playerChoice();
         let compMove = compChoice();
-
+        
         if (playerMove === compMove){
-            alert('Make an another move. Both of moves are similar.');
-            continue;
-        } else if (moveVars.indexOf(playerMove) === moveVars.indexOf(compMove) + 1){
-            compPoints += 1;
-            alert(`Computer won this round: Current count is ${player}: ${plrPoints}: Computer ${compPoints}`);
-            continue;
-        } else if (moveVars.indexOf(playerMove) === moveVars.indexOf(compMove) - 1){
-            plrPoints += 1; 
-            alert(`You won this round: Current count is ${player}: ${plrPoints}: Computer ${compPoints}`);
-            continue;
+            alert('Make an another move. You have the same result as the computer.');
+        }else if (moveVars.indexOf(playerMove) === moveVars.indexOf(compMove) + 1){
+            compPoints++;
+            alert(`Computer won this round: Current count is ${player}: ${plrPoints} - Computer: ${compPoints}`);
+        }else if (moveVars.indexOf(playerMove) === moveVars.indexOf(compMove) - 1){
+            plrPoints++; 
+            alert(`You won this round: Current count is ${player}: ${plrPoints} - Computer: ${compPoints}`);
+        }else if (moveVars.lastIndexOf(playerMove) === moveVars.indexOf(compMove) + 1){
+            compPoints++;
+            alert(`Computer won this round: Current count is ${player}: ${plrPoints} - Computer: ${compPoints}`);
+        }else if (moveVars.lastIndexOf(playerMove) === moveVars.indexOf(compMove) + 2){
+            plrPoints++;
+            alert(`You won this round: Current count is ${player}: ${plrPoints} - Computer: ${compPoints}`);
         }
     }
 
+    let message;
+
     if (compPoints === 3){
-        alert(`Sorry. You lost this game. Count - You: ${plrPoints}: Computer ${compPoints}`)
+        message = alert(`Sorry. You lost this game. Count - You: ${plrPoints} - Computer: ${compPoints}`);
     } else if (plrPoints === 3){
-        alert(`Congratulations. You won this game. Count - You: ${plrPoints}: Computer ${compPoints}`);
+        message = alert(`Congratulations. You won this game. Count - You: ${plrPoints} - Computer: ${compPoints}`);
     }
 
-    let conf = confirm('Do you want to start new game?');
-
-    if (conf === true){
-        return gameStarted();
-    }
-
+    return message;
 }
 
 const moveVars = ['rock', 'scissors', 'paper', 'rock'];
-
-
 let player = enterUserName();
+let game = fighting();
 
-gameStarted();
+let newGameConfirm = confirm('Do you want to start new game?');
+
+if (!!newGameConfirm){
+    fighting();
+} else{
+    process.abort();
+}
 
 
